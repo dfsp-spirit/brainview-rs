ba45889a902ef1f11fa26de8f5b6fed6646fe0e4
Tim Schäfer <ts+code@rcmd.org>
2021-03-13 23:13:20 +0100

    get runnable version

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     let scene_center = vec3(0.0, 0.0, 0.0);
     let scene_radius = 300.0; // TODO: compute mesh max entend
     let mut camera = Camera::new_perspective(&context, scene_center + scene_radius * vec3(0.6, 0.3, 1.0).normalize(), scene_center, vec3(0.0, 1.0, 0.0),
-                                             degrees(45.0), window.viewport().aspect(), 0.1, 1000.0);
+                                             degrees(45.0), window.viewport().aspect(), 0.1, 1000.0).unwrap();
 
     // Read brain meshes for both hemis.
     let lh_white = read_surf("resources/subjects_dir/subject1/surf/lh.white").unwrap();
@@ -50,21 +50,21 @@ fn main() {
     let mut rotating = false;
     window.render_loop(move |frame_input|
     {
-        camera.set_aspect(frame_input.viewport.aspect());
+        camera.set_aspect(frame_input.viewport.aspect()).unwrap();
 
         for event in frame_input.events.iter() {
             match event {
                 Event::MouseClick { state, button, .. } => {
                     rotating = *button == MouseButton::Left && *state == State::Pressed;
                 },
-                Event::MouseMotion { delta, .. } => {
-                    if rotating {
-                        camera.rotate_around_up(delta.0 as f32, delta.1 as f32);
-                    }
-                },
-                Event::MouseWheel { delta, .. } => {
-                    camera.zoom(delta.1 as f32);
-                },
+                //Event::MouseMotion { delta, .. } => {
+                //    if rotating {
+                //        camera.rotate_around_up(delta.0 as f32, delta.1 as f32);
+                //    }
+                //},
+                //Event::MouseWheel { delta, .. } => {
+                //    camera.zoom(delta.1 as f32).unwrap();
+                //},
                 _ => {}
             }
         }
@@ -75,13 +75,13 @@ fn main() {
             rh_mesh.render_color(RenderStates::default(), frame_input.viewport, &transformation, &camera)?;
             Ok(())
         }).unwrap();
-
-        #[cfg(target_arch = "x86_64")]
-        if let Some(ref path) = screenshot_path {
-            use three_d::io::*;
-            let pixels = Screen::read_color(&context, frame_input.viewport).unwrap();
-            Saver::save_pixels(path, &pixels, frame_input.viewport.width, frame_input.viewport.height).unwrap();
-            std::process::exit(1);
+        
+        if args.len() > 1 {
+            // To automatically generate screenshots of the examples, can safely be ignored.
+            FrameOutput {screenshot: Some(args[1].clone().into()), exit: true, ..Default::default()}
+        } else {
+            // Returns default frame output to end the frame
+            FrameOutput::default()
         }
     }).unwrap();
 }
