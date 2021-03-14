a69f615f66d4f9c273cf8f204fd8e5bae75d05f2
Tim Schäfer <ts+code@rcmd.org>
2021-03-14 20:45:01 +0100

    allow turning off auto-rotation

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ use libbrainview::ColoredBrainMesh;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let screenshot_path = if args.len() > 1 { Some(args[1].clone()) } else {None};
 
     let window = Window::new("Cortical thickness", Some((1280, 720))).unwrap();
     let context = window.gl();
 
     let scene_center = vec3(0.0, 0.0, 0.0);
     let scene_radius = 300.0; // TODO: compute mesh max entend
-    let mut camera = Camera::new_perspective(&context, scene_center + scene_radius * vec3(0.6, 0.3, 1.0).normalize(), scene_center, vec3(0.0, 1.0, 0.0),
-                                             degrees(45.0), window.viewport().aspect(), 0.1, 1000.0).unwrap();
+    let mut camera = CameraControl::new(Camera::new_perspective(&context, scene_center + scene_radius * vec3(0.6, 0.3, 1.0).normalize(), scene_center, vec3(0.0, 1.0, 0.0),
+                                             degrees(45.0), window.viewport().aspect(), 0.1, 1000.0).unwrap());
 
     // Read brain meshes for both hemis.
     let lh_white = read_surf("resources/subjects_dir/subject1/surf/lh.white").unwrap();
@@ -30,10 +29,6 @@ fn main() {
     let lh_brain_colors = colors_as_u8_4(apply_colormap(scale_to_01(lh_curv.data), gradient), 255 as u8);
     let rh_brain_colors = colors_as_u8_4(apply_colormap(scale_to_01(rh_curv.data), gradient), 255 as u8);
 
-
-    //let lh_brain_colors: Vec<u8> = vec![255; lh_white.mesh.vertices.len() * 4];
-    //let rh_brain_colors: Vec<u8> = vec![255; rh_white.mesh.vertices.len() * 4];
-
     let lh_cbmesh = ColoredBrainMesh::from_brainmesh_and_colors(&lh_white.mesh, lh_brain_colors).unwrap();
     let rh_cbmesh = ColoredBrainMesh::from_brainmesh_and_colors(&rh_white.mesh, rh_brain_colors).unwrap();
 
@@ -41,7 +36,9 @@ fn main() {
     let rh_mesh = mesh_from_colored_brain_mesh(&rh_cbmesh, &context).unwrap();
 
     // main loop
-    let mut rotating = false;
+    let mut cam_rotating = false;   // Whether the user is rotating the cam with the mouse.
+    let mut do_transform = true;   // Whether the brain mesh is auto-rotating. Can be toggled on/off.
+    let mouse_rotate_speed_factor : f32 = 3.0;
     window.render_loop(move |frame_input|
     {
         camera.set_aspect(frame_input.viewport.aspect()).unwrap();
@@ -49,33 +46,33 @@ fn main() {
         for event in frame_input.events.iter() {
             match event {
                 Event::MouseClick { state, button, .. } => {
-                    rotating = *button == MouseButton::Left && *state == State::Pressed;
+                    cam_rotating = *button == MouseButton::Left && *state == State::Pressed;
                 },
-                //Event::MouseMotion { delta, .. } => {
-                //    if rotating {
-                //        camera.rotate_around_up(delta.0 as f32, delta.1 as f32);
-                //    }
-                //},
-                //Event::MouseWheel { delta, .. } => {
-                //    camera.zoom(delta.1 as f32).unwrap();
-                //},
+                Event::MouseMotion { delta, .. } => {
+                    if cam_rotating {
+                        camera.rotate_around_up((delta.0 as f32) * mouse_rotate_speed_factor, (delta.1 as f32) * mouse_rotate_speed_factor).unwrap();
+                    }
+                },
+                Event::MouseWheel { delta, .. } => {
+                    camera.zoom(delta.1 as f32).unwrap();
+                },
+                Event::Key { state, kind, .. } => {
+                    if *kind == Key::R && *state == State::Pressed
+                    {
+                        do_transform = !do_transform;
+                    }
+                },
                 _ => {}
             }
         }
 
         Screen::write(&context, &ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0), || {
-            let transformation = Mat4::from_angle_y(radians((frame_input.accumulated_time * 0.0005) as f32));
+            let transformation = if do_transform { Mat4::from_angle_y(radians((frame_input.accumulated_time * 0.0005) as f32)) } else { Mat4::identity()};
             lh_mesh.render_color(RenderStates::default(), frame_input.viewport, &transformation, &camera)?;
             rh_mesh.render_color(RenderStates::default(), frame_input.viewport, &transformation, &camera)?;
             Ok(())
         }).unwrap();
         
-        if args.len() > 1 {
-            // To automatically generate screenshots of the examples, can safely be ignored.
-            FrameOutput {screenshot: Some(args[1].clone().into()), exit: true, ..Default::default()}
-        } else {
-            // Returns default frame output to end the frame
-            FrameOutput::default()
-        }
+        FrameOutput::default()
     }).unwrap();
 }
