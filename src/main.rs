3627d0a03f971541452554d08547c2b2b5d763d2
Tim Schäfer <ts+code@rcmd.org>
2021-03-14 21:59:53 +0100

    add comments

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use libbrainview::{ColoredBrainMesh, color_from_data, scene};
 fn main() {
     //let args: Vec<String> = std::env::args().collect();
 
-
     // Read brain meshes for both hemis.
     let lh_white = read_surf("resources/subjects_dir/subject1/surf/lh.white").unwrap();
     let rh_white = read_surf("resources/subjects_dir/subject1/surf/rh.white").unwrap();
@@ -15,12 +14,14 @@ fn main() {
     let lh_curv = read_curv("resources/subjects_dir/subject1/surf/lh.thickness").unwrap();
     let rh_curv = read_curv("resources/subjects_dir/subject1/surf/rh.thickness").unwrap();
 
-    // generate colors from morph data
+    // Generate colors from morph data
     let lh_brain_colors = color_from_data(lh_curv.data);
     let rh_brain_colors = color_from_data(rh_curv.data);
 
+    // Create ColoredBrainMeshes from the data.
     let lh_cbmesh = ColoredBrainMesh::from_brainmesh_and_colors(&lh_white.mesh, lh_brain_colors).unwrap();
     let rh_cbmesh = ColoredBrainMesh::from_brainmesh_and_colors(&rh_white.mesh, rh_brain_colors).unwrap();
 
+    // Visualize the ColoredBrainMeshes.
     scene(vec![lh_cbmesh, rh_cbmesh])
 }
